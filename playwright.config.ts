09835506d883db2948ea894f0116fec3042aa161
njvision <natjel1982@gmail.com>
2024-01-09 13:37:02 +0200

    comment configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
+    // forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    // retries: process.env.CI ? 2 : 0,
-    retries: 2,
+    retries: process.env.CI ? 2 : 1,
+    // retries: 2,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
